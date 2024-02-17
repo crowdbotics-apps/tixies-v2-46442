9bb46a8173c63b67b9970ff18e5920e0508c1aa2
Crowdbotics <team@crowdbotics.com>
2024-02-17 17:32:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tixies-v2-46442",
+  url: "https://tixies-v2-46442.botics.co",
+  api: "https://tixies-v2-46442.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
